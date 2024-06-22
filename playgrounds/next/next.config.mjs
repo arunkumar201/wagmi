ffb96cfdcbd4e37bf3d8966222fe8f95912b8e72
arunp <arunprajapati9825@gmail.com>
2024-06-22 08:59:37 +0530

    ignore linting,typechecking,added wagmi

--- playgrounds/next/next.config.mjs
+++ playgrounds/next/next.config.mjs
@@ -1,20 +1,33 @@
-import bundleAnalyzer from '@next/bundle-analyzer'
+import bundleAnalyzer from "@next/bundle-analyzer";
 
 const withBundleAnalyzer = bundleAnalyzer({
-  enabled: process.env.ANALYZE === 'true',
-})
+	enabled: process.env.ANALYZE === "true",
+});
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    externalDir: true,
-  },
-  webpack(config) {
-    config.resolve.extensionAlias = {
-      '.js': ['.js', '.ts'],
-    }
-    return config
-  },
-}
+	experimental: {
+		externalDir: true,
+	},
+	eslint: {
+		// Warning: This allows production builds to successfully complete even if
+		// your project has ESLint errors.
+		ignoreDuringBuilds: true,
+	},
+	typescript: {
+		// !! WARN !!
+		// Dangerously allow production builds to successfully complete even if
+		// your project has type errors.
+		// !! WARN !!
+		ignoreBuildErrors: true,
+	},
+	webpack(config) {
+		config.resolve.extensionAlias = {
+			".js": [".js", ".ts"],
+		};
 
-export default withBundleAnalyzer(nextConfig)
+		return config;
+	},
+};
+
+export default withBundleAnalyzer(nextConfig);
