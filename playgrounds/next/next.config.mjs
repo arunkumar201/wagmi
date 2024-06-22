168a7eec05d40ee8a8f65b4eaf700c3cfc11de8c
arunp <arunprajapati9825@gmail.com>
2024-06-22 09:15:27 +0530

    update nextconfig - ignore the linting and ts check

--- playgrounds/next/next.config.mjs
+++ playgrounds/next/next.config.mjs
@@ -1,33 +1,33 @@
-import bundleAnalyzer from "@next/bundle-analyzer";
+import bundleAnalyzer from '@next/bundle-analyzer'
 
 const withBundleAnalyzer = bundleAnalyzer({
-	enabled: process.env.ANALYZE === "true",
-});
+  enabled: process.env.ANALYZE === 'true',
+})
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	experimental: {
-		externalDir: true,
-	},
-	eslint: {
-		// Warning: This allows production builds to successfully complete even if
-		// your project has ESLint errors.
-		ignoreDuringBuilds: true,
-	},
-	typescript: {
-		// !! WARN !!
-		// Dangerously allow production builds to successfully complete even if
-		// your project has type errors.
-		// !! WARN !!
-		ignoreBuildErrors: true,
-	},
-	webpack(config) {
-		config.resolve.extensionAlias = {
-			".js": [".js", ".ts"],
-		};
+  experimental: {
+    externalDir: true,
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+  webpack(config) {
+    config.resolve.extensionAlias = {
+      '.js': ['.js', '.ts'],
+    }
 
-		return config;
-	},
-};
+    return config
+  },
+}
 
-export default withBundleAnalyzer(nextConfig);
+export default withBundleAnalyzer(nextConfig)
